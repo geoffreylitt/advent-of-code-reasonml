ac89db70443205d3f6131265455fc6d6e54cf258
Geoffrey Litt <gklitt@gmail.com>
2019-12-02 21:56:40 -0500

    day 2

--- src/day2/solution2.bs.js
+++ src/day2/solution2.bs.js
@@ -4,6 +4,7 @@
 var Curry = require("bs-platform/lib/js/curry.js");
 var Caml_int32 = require("bs-platform/lib/js/caml_int32.js");
 var Tablecloth = require("tablecloth-bucklescript/src/tablecloth.bs.js");
+var Caml_option = require("bs-platform/lib/js/caml_option.js");
 var Caml_exceptions = require("bs-platform/lib/js/caml_exceptions.js");
 
 var input = "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,19,6,23,2,6,23,27,2,27,9,31,1,5,31,35,1,35,10,39,2,39,9,43,1,5,43,47,2,47,10,51,1,51,6,55,1,5,55,59,2,6,59,63,2,63,6,67,1,5,67,71,1,71,9,75,2,75,10,79,1,79,5,83,1,10,83,87,1,5,87,91,2,13,91,95,1,95,10,99,2,99,13,103,1,103,5,107,1,107,13,111,2,111,9,115,1,6,115,119,2,119,6,123,1,123,6,127,1,127,9,131,1,6,131,135,1,135,2,139,1,139,10,0,99,2,0,14,0";
@@ -112,18 +113,69 @@ function interpret(_program, _index) {
   };
 }
 
-function run(input) {
-  return interpret(parse(input), 0);
+function getExn(option) {
+  if (option !== undefined) {
+    return Caml_option.valFromOption(option);
+  } else {
+    throw MissingValue;
+  }
 }
 
-var output = interpret(parse(input), 0);
+function run(input) {
+  var func = Tablecloth.$$Array.get;
+  var arg = interpret(input, 0);
+  return getExn((function (param) {
+                  return Curry._2(func, param, arg);
+                })(0));
+}
 
-console.log(output);
+var program = parse(input);
+
+console.log(run(program));
+
+function runWithInputs(noun, verb) {
+  var program = parse(input);
+  Tablecloth.$$Array.set(1, noun, program);
+  Tablecloth.$$Array.set(2, verb, program);
+  return run(program);
+}
+
+function range(max) {
+  return Tablecloth.List.initialize(max, (function (n) {
+                return n + 1 | 0;
+              }));
+}
+
+function findSolution(_noun, _verb) {
+  while(true) {
+    var verb = _verb;
+    var noun = _noun;
+    var result = runWithInputs(noun, verb);
+    if (result === 19690720) {
+      return /* tuple */[
+              noun,
+              verb
+            ];
+    } else if (verb < 100) {
+      _verb = verb + 1 | 0;
+      continue ;
+    } else {
+      _verb = 0;
+      _noun = noun + 1 | 0;
+      continue ;
+    }
+  };
+}
+
+console.log(findSolution(0, 0));
 
 var testInput = "1,1,1,4,99,5,6,0,99";
 
+var goal = 19690720;
+
 exports.input = input;
 exports.testInput = testInput;
+exports.goal = goal;
 exports.parse = parse;
 exports.UnknownOpcode = UnknownOpcode;
 exports.MissingValue = MissingValue;
@@ -134,6 +186,10 @@ exports.setPointer = setPointer;
 exports.add = add;
 exports.multiply = multiply;
 exports.interpret = interpret;
+exports.getExn = getExn;
 exports.run = run;
-exports.output = output;
-/* output Not a pure module */
+exports.program = program;
+exports.runWithInputs = runWithInputs;
+exports.range = range;
+exports.findSolution = findSolution;
+/* program Not a pure module */
--- src/day2/solution2.re
+++ src/day2/solution2.re
@@ -2,8 +2,9 @@ open Tablecloth
 
 let input = "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,19,6,23,2,6,23,27,2,27,9,31,1,5,31,35,1,35,10,39,2,39,9,43,1,5,43,47,2,47,10,51,1,51,6,55,1,5,55,59,2,6,59,63,2,63,6,67,1,5,67,71,1,71,9,75,2,75,10,79,1,79,5,83,1,10,83,87,1,5,87,91,2,13,91,95,1,95,10,99,2,99,13,103,1,103,5,107,1,107,13,111,2,111,9,115,1,6,115,119,2,119,6,123,1,123,6,127,1,127,9,131,1,6,131,135,1,135,2,139,1,139,10,0,99,2,0,14,0";
 let testInput = "1,1,1,4,99,5,6,0,99";
+let goal = 19690720;
 
-let parse = (str) => {
+let parse = (str) : array(int) => {
   String.split(str, ~on=",")
     // TODO: Weird to default to 0 here. Would be better to raise an exception
     -> List.map(~f= (str) => Result.withDefault(String.to_int(str), ~default=0))
@@ -80,10 +81,49 @@ let rec interpret = (program, ~index) => {
   }
 };
 
-let run = (input) => {
-  parse(input) -> interpret(~index=0)
+// Convert an option to a value, or raise
+let getExn = (option) => {
+  switch(option) {
+    | Some(res) => res
+    | None => raise(MissingValue)
+  };
 }
 
-let output = run(input)
-Js.log(output)
+let run = (input) : int => {
+  interpret(input, ~index=0) -> Array.get(~index = 0) -> getExn
+}
+
+// part 1
+
+let program = parse(input);
+Js.log(run(program));
+
+
+// part 2
+
+let runWithInputs = (noun: int, verb: int) => {
+  let program = parse(input);
+  Array.set(program, ~index=1, ~value=noun);
+  Array.set(program, ~index=2, ~value=verb);
+  run(program)
+};
+
+let range = (max) => List.initialize(max, (n) => n + 1);
+
+let rec findSolution = (noun, verb) => {
+  let result = runWithInputs(noun, verb);
+  if (result == goal) {
+    (noun, verb)
+  } else {
+    if (verb < 100) {
+      findSolution(noun, verb + 1)
+    }
+    else {
+      findSolution(noun + 1, 0)
+    }
+  }
+}
+
+Js.log(findSolution(0, 0));
+
 
